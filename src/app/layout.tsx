67f7892a9a8372ea7e721f5ff3808013bf6da8ce
Abraão Sousa <abraao_andrade_sousa@hotmail.com>
2024-03-27 20:41:29 -0300

    add homepage styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 
  const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['100', '400', '700'],
+  weight: ['100', '400', '500','700'],
 })
 
 export const pacifico = Pacifico({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import Image from 'next/image';
+import './home.css'
+
 export default function Home(){
-    return <h1>Oi</h1>
+    return  <>
+            <div className="wrapper">
+                <div className="container-info">
+
+                    <div className='info1'>
+                        <Image src='/pet2.jpg' alt='pet-image1' width={250} height={250}/>
+                        <div className='info1-text'>
+                            <h2>Quem somos nós?</h2>
+                            <p>Nós somos a <span className='infobold'>a.pets</span>. Surgimos da <br />necessidade de encontrar lares para <br />animais em situações de desabrigo <br />de forma fácil, simples e segura!</p>
+                        </div>
+                    </div>
+                    <div className='info2'>
+                        <div className='info2-text'>
+                            <h2>Como funciona?</h2>
+                            <p>Para começar é simples, só cadastrar <br />sua conta e provar sua identidade, que <br />você poderá listar um bichinho, <br />ou encontrar um para chamar de seu!</p>
+                        </div>
+                    <Image src='/pet1.jpg' alt='pet-image1' width={250} height={250}/>
+                    </div>
+                </div>
+                <div className="container-register">
+                    abuble
+                </div>
+            </div>
+            </>
 }
