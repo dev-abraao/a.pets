d3bf8801e5bda3294c4e00e821a140858d967449
Abraão Sousa <abraao_andrade_sousa@hotmail.com>
2024-04-01 12:40:18 -0300

    register styling start

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ export default function Register() {
     return (
         <div className="wrapper">
             <div className="register-container">
-
+                Teste
             </div>
         </div>
     )
